9eb9269c708c6a4557dc7037085332ce1d0e92ee
Tekku <testmail@tekku.pt>
2024-10-20 23:32:29 +0100

    web_portfolio

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+const home = document.getElementById("iIntro-text");
+
+
+home.addEventListener("scroll", (event) => {
+    window.alert("sometext");
+    
+  });
+
+
+  function myFunction(){
+    // window.alert("sometext");
+  }
